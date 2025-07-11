a8811e01321ba3e87194acf96d2748323f7d2853
vtamprateep <vtamprateep@gmail.com>
2025-07-10 22:45:00 -0400

    dynamic length voice recording

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import torch
 import sounddevice as sd
 import numpy as np
+import queue
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 from scipy.io.wavfile import write, read
+from sounddevice import CallbackFlags
+from typing import Any
 
 
 class AudioTranscriberModel:
@@ -66,23 +69,69 @@ class ConversationGeneratorModel:
 
 
 class VoiceRecorder:
+
+    def __init__(self):
+        self.audio_block_queue = queue.Queue()
+
+    def _record_callback(
+        self,
+        indata: np.ndarray,
+        frames: int,
+        time: Any,
+        status: CallbackFlags
+    ) -> None:
+        if status:
+            print(status)
+        self.audio_block_queue.put(indata.copy())
+
+    def _is_silence_detected(
+        self,
+        block: np.ndarray,
+        silence_threshold: float = 0.01
+    ) -> bool:
+        rms = np.sqrt(np.mean(block**2))
+        silence_detected = rms < silence_threshold
+
+        if silence_detected:
+            print("> Silence detected!")
+            return True
+        return False
+
     def record(
-        self, file_name: str = None, sampling_rate=16000, duration: int = 5
+        self,
+        file_name: str = None,
+        sampling_rate: int = 16000,
+        block_duration: float = 0.5,
+        buffer_duration: int = 5,
     ) -> dict:
-        print("Recording...")
-        audio = sd.rec(
-            int(duration * sampling_rate),
+        block_size = int(block_duration * sampling_rate)
+        min_recorded_blocks = int(buffer_duration / block_duration)
+        audio_blocks = []
+
+        print("> Recording stream start...")
+        with sd.InputStream(
             samplerate=sampling_rate,
             channels=1,
-            dtype="float32",
-        )
-        sd.wait()
-        print("Done recording.")
-        if file_name:
-            write(f"{file_name}.wav", sampling_rate, audio)
-            print(f"Saved to {file_name}.wav")
+            blocksize=block_size,
+            callback=self._record_callback
+        ):
+            while True:
+                next_audio_block = self.audio_block_queue.get()
+                audio_blocks.append(next_audio_block)
+                min_recorded_blocks -= 1
 
-        return {"sampling_rate": sampling_rate, "raw": np.array(audio)}
+                print(f"> Total recorded blocks: {len(audio_blocks)}")
+
+                if self._is_silence_detected(next_audio_block) and min_recorded_blocks <= 0:
+                    print("> Recording stopped!")
+                    break
+
+        full_audio_data = np.concatenate(audio_blocks, axis=0)
+
+        if file_name:
+            write(file_name, sampling_rate, full_audio_data)
+
+        return {"sampling_rate": sampling_rate, "raw": full_audio_data}
 
     def read(self, file_name: str) -> dict:
         sample_rate, audio_data = read(f"{file_name}.wav")
@@ -105,10 +154,15 @@ if __name__ == "__main__":
     # print(inference_result)
 
     # Test text generation
-    conversation_generator = ConversationGeneratorModel()
-    for _ in range(10):
-        user_input = input("> User Input (if end, press enter): ")
-        if not user_input:
-            break
+    # conversation_generator = ConversationGeneratorModel()
+    # for _ in range(10):
+    #     user_input = input("> User Input (if end, press enter): ")
+    #     if not user_input:
+    #         break
 
-        print(conversation_generator.run_inference(user_input))
+    #     print(conversation_generator.run_inference(user_input))
+
+    # Test VoiceRecorder
+    voice_recorder = VoiceRecorder()
+    audio_data = voice_recorder.record()
+    print(audio_data)
