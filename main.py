eebdb4731bbc93992190908dae2a7860b7cd8e1a
vtamprateep <vtamprateep@gmail.com>
2025-07-16 18:17:04 -0400

    refactor voice audio class

--- main.py
+++ main.py
@@ -1,86 +1,10 @@
-import sounddevice as sd
-import numpy as np
-import queue
-import math
-from scipy.io.wavfile import write, read
-from sounddevice import CallbackFlags
-from typing import Any
-
 from .util.model import (
     SpeechToTextModel,
     ConversationGeneratorModel,
 )
-
-
-class VoiceRecorder:
-    def __init__(self):
-        self.audio_block_queue = queue.Queue()
-
-    def _record_callback(
-        self, indata: np.ndarray, frames: int, time: Any, status: CallbackFlags
-    ) -> None:
-        if status:
-            print(status)
-        self.audio_block_queue.put(indata.copy())
-
-    def _is_silence_detected(
-        self, block: np.ndarray, silence_threshold: float = 0.01
-    ) -> bool:
-        rms = np.sqrt(np.mean(block**2))
-        silence_detected = rms < silence_threshold
-
-        if silence_detected:
-            print("> Silence detected!")
-            return True
-        return False
-
-    def record(
-        self,
-        file_name: str = None,
-        sampling_rate: int = 16000,
-        block_duration: float = 1.0,
-        buffer_duration: int = 5,
-    ) -> dict:
-        block_size = int(block_duration * sampling_rate)
-        min_recorded_blocks = math.ceil(buffer_duration / block_duration)
-        audio_blocks = []
-
-        print("> Recording stream start...")
-        with sd.InputStream(
-            samplerate=sampling_rate,
-            channels=1,
-            blocksize=block_size,
-            callback=self._record_callback,
-        ):
-            while True:
-                next_audio_block = self.audio_block_queue.get()
-                audio_blocks.append(next_audio_block)
-                min_recorded_blocks -= 1
-
-                print(f"> Total recorded blocks: {len(audio_blocks)}")
-
-                if (
-                    self._is_silence_detected(next_audio_block)
-                    and min_recorded_blocks <= 0
-                ):
-                    print("> Recording stopped!")
-                    break
-
-        full_audio_data = np.concatenate(audio_blocks, axis=0)[:, 0]
-
-        if file_name:
-            write(file_name, sampling_rate, full_audio_data)
-
-        return {"sampling_rate": sampling_rate, "raw": full_audio_data}
-
-    def read(self, file_name: str) -> dict:
-        sample_rate, audio_data = read(f"{file_name}")
-        return {"sampling_rate": sample_rate, "raw": np.array(audio_data)}
-
-    def play(self, file_name: str) -> None:
-        print(f"Playing audio file: {file_name}.wav")
-        sample_rate, audio_data = read(f"{file_name}.wav")
-        sd.play(audio_data, sample_rate, blocking=True)
+from .util.audio import (
+    VoiceRecorder
+)
 
 
 if __name__ == "__main__":
