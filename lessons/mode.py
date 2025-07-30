cabd9964230da13703103e739e57b6e86852a317
vtamprateep <vtamprateep@gmail.com>
2025-07-29 23:56:08 -0400

    mandarin texting mode

--- lessons/mode.py
+++ lessons/mode.py
@@ -1,9 +1,10 @@
+from lessons.types import GrammarItem, VocabularyItem
 from util.audio import VoiceRecorder
 from util.languages import Language, TextTranslator
 from util.model import QwenCausalLM, SpeechToTextModel, TextToSpeechModel
 
 
-class MandarinConversation:
+class MandarinSpeech:
     """Conversation must be in Mandarin."""
 
     def __init__(self):
@@ -37,9 +38,9 @@ class MandarinConversation:
         bot_text_response = self.CONVERSATION_GENERATOR.run_inference(user_text)
         return bot_text_response
 
-    def add_scenario(self, scenario: str) -> None:
+    def add_context(self, context: str) -> None:
         self.CONVERSATION_GENERATOR.add_system_prompt(
-            f"This is the conversation role-playing scenario: {scenario}"
+            f"This is the conversation role-playing scenario: {context}"
         )
 
     def prioritize_vocabulary(self, vocab_arr: list[str]) -> None:
@@ -60,6 +61,73 @@ class MandarinConversation:
                 self._translate_text(bot_text, self.LANGUAGE)
             )
             self.VOICE_RECORDER.play(bot_audio_data)
+
+            transcript.append(
+                {
+                    "user": user_text,
+                    "user_english": self._translate_text(user_text, Language.ENGLISH),
+                    "bot": bot_text,
+                    "bot_english": self._translate_text(bot_text, Language.ENGLISH),
+                }
+            )
+            count_turns += 1
+
+        return transcript
+
+
+class MandarinText:
+    """Class that you can type at a bot back and forth."""
+
+    def __init__(self):
+        self.LANGUAGE = Language.MANDARIN
+        QwenCausalLM.get_instance()
+        QwenCausalLM.add_system_prompt(
+            (
+                "You are my Mandarin practice partner."
+                " You help me by holding conversations in Mandarin, prompting"
+                " me to use key vocabulary and grammar patterns."
+            )
+        )
+
+    def _translate_text(self, input: str, target: Language) -> str:
+        return TextTranslator.translate(input, target)
+
+    def _get_user_response(self) -> str:
+        return input("> You: ")
+
+    def _get_bot_response(self, user_text: str) -> str:
+        bot_text_response = QwenCausalLM.run_inference(user_text)
+        return bot_text_response
+
+    def add_scenario(self, scenario: str) -> None:
+        QwenCausalLM.add_system_prompt(
+            f"This is the conversation role-playing scenario from the perspective of the user: {scenario}"
+        )
+
+    def add_vocabulary(self, vocab_arr: list[VocabularyItem]) -> None:
+        QwenCausalLM.add_system_prompt(
+            (
+                "Prioritize using the following vocabulary when it makes sense:"
+                f" {','.join(item['chinese'] for item in vocab_arr)}"
+            )
+        )
+
+    def add_grammar(self, grammar_arr: list[GrammarItem]) -> None:
+        QwenCausalLM.add_system_prompt(
+            (
+                f"Prioritize prompting me to use the following grammar patterns"
+                " in my response when it makes sense:"
+                f" {','.join(item['structure'] for item in grammar_arr)}"
+            )
+        )
+
+    def start_session(self, turns: int) -> list[dict[str, str]]:
+        count_turns = 0
+        transcript = []
+        while count_turns < turns:
+            user_text = self._get_user_response()
+            bot_text = self._get_bot_response(user_text)
+            print(f"> Bot: {bot_text}")
 
             transcript.append(
                 {
