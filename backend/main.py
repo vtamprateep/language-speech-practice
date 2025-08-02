a15fa3aa5bee2b67dcf3a464a89645efb019c86a
vtamprateep <vtamprateep@gmail.com>
2025-08-02 18:29:53 -0400

    basic api endpoints prompt model

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,38 @@
-from typing import Union
+from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from lessons.beginner import AT_RESTAURANT
+from lessons.mode import MandarinText
+from pydantic import BaseModel
 
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+text_dialogue_engine = dict()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    text_dialogue_engine["mandarin_text"] = MandarinText()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/configure/lesson")
+async def configure_lesson():
+    from lessons.beginner import AT_RESTAURANT
+
+    text_dialogue_engine["mandarin_text"].add_grammar(AT_RESTAURANT["grammar"])
+    text_dialogue_engine["mandarin_text"].add_scenario(AT_RESTAURANT["scenarios"])
+    text_dialogue_engine["mandarin_text"].add_vocabulary(AT_RESTAURANT["vocabulary"])
+    return "Lesson configured!"
+
+
+class ChatInput(BaseModel):
+    text: str
+
+
+@app.post("/user/chat")
+async def chat(request: Request, input: ChatInput):
+    prompt = input.text
+    bot_response = text_dialogue_engine["mandarin_text"]._get_bot_response(prompt)
+    return bot_response
