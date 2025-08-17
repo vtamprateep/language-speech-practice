f216be1e3133b86dee55de205597d3e983567f42
vtamprateep <vtamprateep@gmail.com>
2025-08-16 22:15:40 -0400

    endpoint for text similarity

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,21 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from lessons.beginner import AT_RESTAURANT
 from lessons.mode import MandarinText
 from pydantic import BaseModel
+from util.model import SemanticMatcher
 
-text_dialogue_engine = dict()
+# text_dialogue_engine = dict()
+core_models = dict()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    text_dialogue_engine["mandarin_text"] = MandarinText()
+    # text_dialogue_engine["mandarin_text"] = MandarinText()
+    core_models["SemanticMatcher"] = SemanticMatcher()
     yield
 
 
@@ -40,8 +43,26 @@ class ChatInput(BaseModel):
     text: str
 
 
-@app.post("/bot/chat")
-async def chat(request: Request, input: ChatInput):
-    prompt = input.text
-    bot_response = text_dialogue_engine["mandarin_text"]._get_bot_response(prompt)
-    return JSONResponse(content={"text": bot_response})
+# @app.post("/bot/chat")
+# async def chat(request: Request, input: ChatInput):
+#     prompt = input.text
+#     bot_response = text_dialogue_engine["mandarin_text"]._get_bot_response(prompt)
+#     return JSONResponse(content={"text": bot_response})
+
+
+class TextComparison(BaseModel):
+    text_1: str
+    text_2: str
+
+
+@app.get("/calculate_similarity")
+async def calculate_similarity(body: TextComparison):
+    print(body)
+    model = core_models["SemanticMatcher"]
+    text_1 = body.text_1
+    text_2 = body.text_2
+    return JSONResponse(
+        content={
+            "score": str(model.get_similarity(text_1, text_2))
+        }
+    )
