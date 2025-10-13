7540c910b083a94fa2a724a63524533b0f22c8cc
vtamprateep <vtamprateep@gmail.com>
2025-10-13 15:25:50 -0400

    add logging

--- backend/app/api/v1/endpoints.py
+++ backend/app/api/v1/endpoints.py
@@ -1,17 +1,16 @@
 import io
+import logging
 
 import numpy as np
-
-# import soundfile as sf  # type: ignore
 from fastapi import APIRouter, Depends, File, Form, UploadFile
 from fastapi.responses import JSONResponse, StreamingResponse
 from pydantic import BaseModel
 
-# from pydub import AudioSegment  # type: ignore
 from app.dependencies import get_clients, get_models
 from app.util.languages import Language
 from app.util.model import AudioData
 
+LOG = logging.getLogger(__name__)
 router = APIRouter()
 
 
@@ -23,11 +22,13 @@ class TextTranslate(BaseModel):
 
 @router.post("/api/v1/translate_text")
 async def translate_text(body: TextTranslate, model=Depends(get_models)):
+    translated_text = model["TextTranslator"].translate(
+        text=body.text, source=body.sourceLang, target=body.targetLang
+    )
+    LOG.info(f"text: {translated_text}")
     return JSONResponse(
         content={
-            "text": model["TextTranslator"].translate(
-                text=body.text, source=body.sourceLang, target=body.targetLang
-            )
+            "text": translated_text
         }
     )
 
@@ -45,6 +46,7 @@ async def calculate_similarity(body: TextComparison, client=Depends(get_clients)
         [body.text_2],
         model="sentence-transformers/all-MiniLM-L6-v2"
     )
+    LOG.info(f"score: {str(score[0])}")
     return JSONResponse(
         content={
             "score": str(score[0])
@@ -55,29 +57,3 @@ async def calculate_similarity(body: TextComparison, client=Depends(get_clients)
 class TTSRequest(BaseModel):
     text: str
     language: str
-
-
-# @router.post("/api/v1/generate_audio")
-# async def generate_audio(body: TTSRequest, model=Depends(get_models)):
-#     audio_data = model["KokoroModel"].run_inference(body.text, body.language)
-
-#     # De-normalize audio array
-#     denormalized_audio_data = (audio_data.raw * 32767).astype("int16")
-
-#     audio_segment = AudioSegment(
-#         data=denormalized_audio_data,
-#         frame_rate=audio_data.sampling_rate,
-#         sample_width=2,  # 16-bit
-#         channels=1,
-#     )
-
-#     # Write to an in-memory buffer
-#     buffer = io.BytesIO()
-#     audio_segment.export(out_f=buffer, format="webm")
-#     buffer.seek(0)
-
-#     return StreamingResponse(
-#         content=buffer,
-#         media_type="audio/webm",
-#         headers={"Content-Disposition": "attachment; filename=output.webm"},
-#     )
