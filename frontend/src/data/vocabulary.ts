46e8238cc469f146a9de97e7288548abde3d2ad5
vtamprateep <vtamprateep@gmail.com>
2025-10-19 08:08:21 -0400

    fix keys

--- frontend/src/data/vocabulary.ts
+++ frontend/src/data/vocabulary.ts
@@ -1,45 +1,8 @@
-import rawData1 from './vocabulary_raw/hsk_n1.json' assert { type:"json" };
-import rawData2 from './vocabulary_raw/hsk_n2.json' assert { type:"json" };
-import rawData3 from './vocabulary_raw/hsk_n3.json' assert { type:"json" };
-import rawData4 from './vocabulary_raw/hsk_n4.json' assert { type:"json" };
-import rawData5 from './vocabulary_raw/hsk_n5.json' assert { type:"json" };
-import rawData6 from './vocabulary_raw/hsk_n6.json' assert { type:"json" };
-import rawData7 from './vocabulary_raw/hsk_n7+.json' assert { type:"json" };
-
 import mandarinVocab1 from './vocabulary_raw/mandarin_vocabulary_level_1.json' assert { type:'json'};
 import mandarinVocab2 from './vocabulary_raw/mandarin_vocabulary_level_2.json' assert { type:'json'};
 import mandarinVocab3 from './vocabulary_raw/mandarin_vocabulary_level_3.json' assert { type:'json'};
 import mandarinVocab4 from './vocabulary_raw/mandarin_vocabulary_level_4.json' assert { type:'json'};
 import mandarinVocab5 from './vocabulary_raw/mandarin_vocabulary_level_5.json' assert { type:'json'};
-
-
-// For https://github.com/drkameleon/complete-hsk-vocabulary
-export interface VocabularyForm { 
-    traditional: string;
-    transcriptions: {
-        pinyin: string;
-        bopomofo: string;
-    }; 
-    meanings: string[];
-    classifiers: string[];
-} 
-
-export interface VocabularyItem { 
-    simplified: string;
-    radical: string;
-    level: string[];
-    frequency: number;
-    pos: string[];
-    forms: VocabularyForm[];
-}
-
-export const vocabulary_1: VocabularyItem[] = rawData1 as VocabularyItem[];
-export const vocabulary_2: VocabularyItem[] = rawData2 as VocabularyItem[];
-export const vocabulary_3: VocabularyItem[] = rawData3 as VocabularyItem[];
-export const vocabulary_4: VocabularyItem[] = rawData4 as VocabularyItem[];
-export const vocabulary_5: VocabularyItem[] = rawData5 as VocabularyItem[];
-export const vocabulary_6: VocabularyItem[] = rawData6 as VocabularyItem[];
-export const vocabulary_7: VocabularyItem[] = rawData7 as VocabularyItem[];
 
 
 // For https://www.roc-taiwan.org/at_de/post/634.html
@@ -51,6 +14,7 @@ export interface Vocabulary {
     level: number;
     topicEnglish: string | null;
     vocabularyEnglish: string;
+    id: number;
 }
 
 export const tradVocabulary1: Vocabulary[] = mandarinVocab1 as Vocabulary[];
