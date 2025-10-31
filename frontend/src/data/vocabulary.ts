1e336c68b7a46661cfd5a22956692fa43af44c59
vtamprateep <vtamprateep@gmail.com>
2025-10-30 22:19:26 -0400

    update data load

--- frontend/src/data/vocabulary.ts
+++ frontend/src/data/vocabulary.ts
@@ -1,8 +1,4 @@
-import mandarinVocab1 from './vocabulary_raw/mandarin_vocabulary_level_1.json' assert { type:'json'};
-import mandarinVocab2 from './vocabulary_raw/mandarin_vocabulary_level_2.json' assert { type:'json'};
-import mandarinVocab3 from './vocabulary_raw/mandarin_vocabulary_level_3.json' assert { type:'json'};
-import mandarinVocab4 from './vocabulary_raw/mandarin_vocabulary_level_4.json' assert { type:'json'};
-import mandarinVocab5 from './vocabulary_raw/mandarin_vocabulary_level_5.json' assert { type:'json'};
+import vocabularyData from './vocabulary_raw/vocabulary_data.json' assert { type:'json'};
 
 
 // For https://www.roc-taiwan.org/at_de/post/634.html
@@ -17,8 +13,4 @@ export interface Vocabulary {
     id: number;
 }
 
-export const tradVocabulary1: Vocabulary[] = mandarinVocab1 as Vocabulary[];
-export const tradVocabulary2: Vocabulary[] = mandarinVocab2 as Vocabulary[];
-export const tradVocabulary3: Vocabulary[] = mandarinVocab3 as Vocabulary[];
-export const tradVocabulary4: Vocabulary[] = mandarinVocab4 as Vocabulary[];
-export const tradVocabulary5: Vocabulary[] = mandarinVocab5 as Vocabulary[];
+export const allVocabulary: Vocabulary[] = vocabularyData as Vocabulary[];
