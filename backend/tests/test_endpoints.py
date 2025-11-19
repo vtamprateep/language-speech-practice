903d1697038606a68985a3c1cb18c3fb8aad9dc9
vtamprateep <vtamprateep@gmail.com>
2025-11-19 08:05:06 -0500

    get top n vocab

--- backend/tests/test_endpoints.py
+++ backend/tests/test_endpoints.py
@@ -13,6 +13,18 @@ app.dependency_overrides[get_clients] = lambda: mock_clients
 test_client = TestClient(app)
 
 
+# Utility function to mock chain of calls in supabase client
+def create_supabase_client_mock(mock_methods: list[str], mock_attrs: list[str]) -> MagicMock:
+    mock_supabase_client = MagicMock()
+    for method in mock_methods:
+        getattr(mock_supabase_client, method).return_value = mock_supabase_client
+
+    for attr in mock_attrs:
+        setattr(mock_supabase_client, attr, mock_supabase_client)
+
+    return mock_supabase_client
+
+
 def test_translate_text():
     # Set-up mock
     mock_translator = MagicMock()
@@ -81,3 +93,34 @@ def test_get_vocabulary_by_id():
 
     mock_supabase_client.table.assert_called_with("vocabulary")
     mock_supabase_client.in_.assert_called_with("id", [0, 1])
+
+
+def test_get_vocabulary_top_n_frequency():
+    # Mock client and chain of calls
+    mock_supabase_client = create_supabase_client_mock([
+        "table",
+        "select",
+        "is_",
+        "order",
+        "limit",
+        "execute"
+    ], ["not_"])
+
+    mock_clients.__getitem__.return_value = mock_supabase_client
+
+    # Call with no value sets default n=10
+    test_client.get(
+        url="/api/v1/get_vocabulary_top_n_frequency",
+    )
+
+    mock_supabase_client.table.assert_called_with("vocabulary")
+    mock_supabase_client.is_.assert_called_with("relative_freq_pct", "null")
+    mock_supabase_client.order.assert_called_with("relative_freq_pct", desc=True)
+
+    # Call with value sets n appropriately
+    test_client.get(
+        url="/api/v1/get_vocabulary_top_n_frequency",
+        params={"n": 5}
+    )
+
+    mock_supabase_client.limit.assert_called_with(5)
