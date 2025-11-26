7c446a296afb7bfef4c9d6025f93ccb9fbccca86
Vinyou Tamprateep <vtamprateep@gmail.com>
2025-11-26 13:37:47 -0500

    Login (#21)
    * supabase client lib
    
    * add ui for login and auth
    
    * middleware template
    
    * update login / logout ui
    
    * update type
    
    * update git workflow
    
    * use secrets
    
    * move env to the step
    
    * fix secret name

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { LoginButton } from "@/components/features/auth/login-button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,15 +20,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-white`}
       >
-        {children}
+        <header className="w-full flex justify-end p-4">
+          <LoginButton />
+        </header>
+
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ frontend/src/components/features/auth/login-button.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { LogIn, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import type { User } from "@supabase/supabase-js";
+
+
+export function LoginButton() {
+    const supabase = createClient();
+    const [user, setUser] = useState<User | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        // Get initial user data
+        supabase.auth.getUser().then(({ data }) => setUser(data.user));
+
+        // Set-up listen to track when state changes
+        supabase.auth.onAuthStateChange(
+            (_event, session) => {
+                setUser(session?.user ?? null);
+            }
+        );
+    }, []);
+
+    function handleLoginRedirect() {
+        router.push("/auth/login");
+    }
+
+    async function handleLogout() {
+        await supabase.auth.signOut();
+    }
+
+    if (!user) {
+        return (
+        <button
+            onClick={handleLoginRedirect}
+            className="flex items-center gap-2 px-3 py-2 rounded-xl hover:bg-gray-100"
+        >
+            <LogIn size={18} />
+            <span>Log in</span>
+        </button>
+        );
+    }
+
+    return (
+        <button
+            onClick={handleLogout}
+            className="flex items-center gap-2 px-3 py-2 rounded-xl hover:bg-gray-100"
+        >
+            <LogOut size={18} />
+            <span>Log out</span>
+        </button>
+    );
+}
