25aa6c385e64eb4df8040f0ba831cddba9a74dd6
vtamprateep <vtamprateep@gmail.com>
2025-11-29 20:41:08 -0500

    re-organize functions

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function chooseRandom(options: any[]): any {
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function shuffle(arr: any[]): any[] {
+    return arr.sort(() => 0.5 - Math.random());
+}
