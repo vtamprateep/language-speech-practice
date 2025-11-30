29a43d279e19e3fb42d59dd5591c1b0cd74e56db
vtamprateep <vtamprateep@gmail.com>
2025-11-30 09:29:18 -0500

    rm unused improts

--- frontend/src/components/features/flashcard/container.tsx
+++ frontend/src/components/features/flashcard/container.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Vocabulary } from "@/lib/backend/types";
 import { Button } from "@/components/ui/button";
 import { TypedResponse, MultipleChoiceResponse } from "./response";
--- frontend/src/components/features/flashcard/response.tsx
+++ frontend/src/components/features/flashcard/response.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
