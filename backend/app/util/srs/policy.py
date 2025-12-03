61d169c030a603d5b6a12aa1d1976ee75edf69fa
vtamprateep <vtamprateep@gmail.com>
2025-12-03 08:27:41 -0500

    initial vocab selection policy

---
+++ backend/app/util/srs/policy.py
@@ -0,0 +1,43 @@
+"""Contains policy that determines which vocabulary is returned"""
+
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
+
+@dataclass
+class SRSItem:
+    id: int
+    count_correct: int
+    count_wrong: int
+
+
+class ExponentialSRSPolicy:
+
+    @staticmethod
+    def retrieve_items(
+        self, 
+        items: list[SRSItem], 
+        smooth_factor: int = 5,
+        score_threshold: float | int = 30,
+        N: int = 6
+    ) -> list[int]:
+        """Given number of times the item has been answered correctly / incorrectly,
+        calculate score and return N ids that need to be studied."""
+
+        # Compute correctness ratio and total encounters
+        df = pd.DataFrame(items)
+        df["count_seen"] = df["count_correct"] + df["count_wrong"]
+        df["correct_ratio"] = df["count_correct"] / (df["count_seen"] + smooth_factor)
+
+        # Calculate exponential score
+        df["sqrt_n"] = np.sqrt(df["count_seen"])
+        df["pwr_correct_ratio"] = np.power(df["correct_ratio"], 2)
+        df["score"] = np.exp(df["pwr_correct_ratio"] * df["sqrt_n"])
+
+        # Sort by score descending, throw out high scorers, get top N, return ID
+        df_filtered = df[df["score"] < score_threshold].sort_values("score")
+        df_top_N = df_filtered.head(N)
+        
+        return df_top_N["id"].to_list()
