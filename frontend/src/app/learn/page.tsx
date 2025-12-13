8b8873d81398e2cc7749f35a8754e1c4c4ee9221
vtamprateep <vtamprateep@gmail.com>
2025-12-13 16:49:18 -0500

    refactor, load vocab and progress

--- frontend/src/app/learn/page.tsx
+++ frontend/src/app/learn/page.tsx
@@ -4,10 +4,62 @@ import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import React, { useState, useEffect } from 'react';
 import { Vocabulary } from '@/lib/backend/types';
-import { getVocabularyById, getVocabularyIdByPolicy, getVocabularyTopNFrequency } from '@/lib/backend/backend';
+import { 
+    getVocabularyById,
+    getVocabularyIdByPolicy,
+    getVocabularyTopNFrequency,
+    getVocabularyProgress,
+    putVocabularyProgressNewRecords
+} from '@/lib/backend/backend';
 import { VocabularyFlashcardMasteryContainer } from '@/components/features/flashcard/container';
 import { useUserContext } from '@/context/user';
 import { shuffle } from '@/lib/utils';
+import { VocabularyProgressRecord } from '@/lib/backend/types';
+
+
+async function loadVocabularyProgress(
+    vocabularyIdArr: number[],
+    userId?: string
+): Promise<VocabularyProgressRecord[]> {
+    // If userId is not provided, give blank progress for all vocabulary
+    if (!userId) {
+        const vocabProgress = vocabularyIdArr.map((id) => {
+            return {
+                vocabularyId: id,
+                countCorrect: 0,
+                countWrong: 0
+            }
+        });
+        return vocabProgress;
+    }
+
+    // Otherwise, get vocabulary progress records from database
+    const vocabularyProgressRecord = await getVocabularyProgress(
+        userId,
+        vocabularyIdArr
+    )
+    const foundVocabId = vocabularyProgressRecord.map((v) => v.vocabularyId);
+
+    // For new vocab, existing record may not exists. Create those records.
+    const missingVocabIds = vocabularyIdArr.filter((v) => !foundVocabId.includes(v));
+
+    // If none were missing, return. Otherwise, create those records
+    if (missingVocabIds.length == 0) return vocabularyProgressRecord;
+    const newRecords = await putVocabularyProgressNewRecords(userId, missingVocabIds);
+    return [...vocabularyProgressRecord, ...newRecords];
+}
+
+
+async function loadVocabulary(userId?: string): Promise<Vocabulary[]> {
+    if (userId) {
+        const vocabIdArr = await getVocabularyIdByPolicy(userId)
+        const vocabArr = await getVocabularyById(vocabIdArr);
+        return vocabArr;                
+    } else {
+        const vocabArr = await getVocabularyTopNFrequency(8);
+        return shuffle(vocabArr);
+    }
+} 
 
 
 function HomeButton() {
@@ -34,27 +86,26 @@ export default function FlashcardsLearnPage({ params }: { params: Promise<{ leve
     const { level } = React.use(params);
     const [readyToRender, setReadyToRender] = useState<boolean>(false);
     const [vocabulary, setVocabulary] = useState<Vocabulary[]>([]);
+    const [vocabularyProgress, setVocabularyProgress] = useState<VocabularyProgressRecord[]>([]);
     const [renderHome, setRenderHome] = useState<boolean>(false);
 
     const { user } = useUserContext();
 
-    async function loadVocabulary(userId?: string): Promise<Vocabulary[]> {
-        if (userId) {
-            const vocabIdArr = await getVocabularyIdByPolicy(userId)
-            const vocabArr = await getVocabularyById(vocabIdArr);
-            return vocabArr;                
-        } else {
-            const vocabArr = await getVocabularyTopNFrequency(8);
-            return shuffle(vocabArr);
-        }
-    } 
+    const loadData = async () => {
+        // Load vocabulary
+        const vocabArr = await loadVocabulary(user?.id);
+        setVocabulary(vocabArr);
+
+        // Load vocabulary progress
+        const vocabIdArr = vocabArr.map((entry) => entry.id);
+        const vocabProgressArr = await loadVocabularyProgress(vocabIdArr, user?.id);
+        setVocabularyProgress(vocabProgressArr);
+
+        setReadyToRender(true);
+    }
 
     useEffect(() => {
-        loadVocabulary(user?.id)
-            .then((data) => {
-                setVocabulary(data);
-                setReadyToRender(true);
-            });
+        loadData();
     }, [])
 
     if (!readyToRender) {
