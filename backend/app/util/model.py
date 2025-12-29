83257c3c1f3a7c3bbd59678145a9ca35a41a0763
Vinyou Tamprateep <vtamprateep@gmail.com>
2025-12-29 17:20:52 -0500

    Cleanup (#28)
    * rm unused files
    
    * rm empty line
    
    * update db set-up script
    
    * add vocab seed, rm python scripts
    
    * rm other python stuff
    
    * disable workflow

--- backend/app/util/model.py
+++ backend/app/util/model.py
@@ -1,64 +1,8 @@
-import uuid
-from dataclasses import dataclass
 from enum import Enum
-from typing import Any
 
-import librosa
-import numpy as np
 from deep_translator import GoogleTranslator  # type: ignore
 
-# from kokoro import KPipeline  # type: ignore
-from transformers import (
-    AutoModel,
-    AutoModelForCausalLM,
-    AutoModelForSpeechSeq2Seq,
-    AutoProcessor,
-    AutoTokenizer,
-    BlenderbotForConditionalGeneration,
-    BlenderbotTokenizer,
-    pipeline,
-)
-
 from .languages import Language
-
-
-@dataclass
-class AudioData:
-    sampling_rate: int
-    raw: np.ndarray
-
-
-# class KokoroModel:
-#     """
-#     🇺🇸 'a' => American English, 🇬🇧 'b' => British English
-#     🇪🇸 'e' => Spanish es
-#     🇫🇷 'f' => French fr-fr
-#     🇮🇳 'h' => Hindi hi
-#     🇮🇹 'i' => Italian it
-#     🇯🇵 'j' => Japanese: pip install misaki[ja]
-#     🇧🇷 'p' => Brazilian Portuguese pt-br
-#     🇨🇳 'z' => Mandarin Chinese: pip install misaki[zh]
-#     """
-
-#     LANGUAGE_MODEL_CONFIG = {Language.ENGLISH: "a", Language.MANDARIN: "z"}
-
-#     def _setup_pipeline(self, language: Language):
-#         return KPipeline(lang_code=self.LANGUAGE_MODEL_CONFIG[language])
-
-#     def run_inference(
-#         self,
-#         input: str,
-#         language: Language,
-#         voice: str = "af_heart",
-#         speed: int = 1,
-#         split_pattern: str = r"\n+",
-#     ) -> AudioData:
-#         pipeline = self._setup_pipeline(language)
-#         audio_segments = [
-#             audio for _, _, audio in pipeline(input, voice, speed, split_pattern)
-#         ]
-#         audio_data = np.concatenate(audio_segments)
-#         return AudioData(24000, audio_data)
 
 
 class TextTranslator:
